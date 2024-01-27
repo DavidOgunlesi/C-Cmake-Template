16c779f3a7dc10815a4db892ebb1955df458d564
David Ogunlesi <david.ogunlesi@yahoo.co.uk>
2024-01-27 21:37:30 +0000

    removed unnecessary files

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,6 @@
 ﻿#include <iostream>
-#include "raylib.h"
-#include "core.ecs.h"
 
 int main()
 {
-	InitWindow(800, 600, "My window");
-	SetTargetFPS(60);
-
-	while (!WindowShouldClose())
-	{
-		CreateEntities();
-
-		ClearBackground(BLACK);
-		EndDrawing();
-	}
-
-	CloseWindow();
 	return 0; 
-}
-
-void CreateEntities()
-{
-	ECS::Archetype archetype = ECS::Archetype([sizeof(ECS::TransformComponent)]);
 }
